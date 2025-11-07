341f79531c3a0ccbb4067fe3c206f2c0d419cc66
Titania30-dit <dita.titania1985@gmail.com>
2025-11-07 11:30:06 +0700

    splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'pages/anime_list_page.dart';
+import 'splashscreen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +12,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    final GoRouter router = GoRouter(
+      initialLocation: '/',
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) => const SplashScreen(),
+        ),
+        GoRoute(
+          path: '/home',
+          builder: (context, state) => const AnimeListPage(),
+        ),
+      ],
+    );
+
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      home: const AnimeListPage(),
+      routerConfig: router,
     );
   }
 }
