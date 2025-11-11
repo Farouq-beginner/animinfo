271ae669458e9cb20ade2d11054d9c3f94aaaa60
Farouq-beginner <24111814081@mhs.unesa.ac.id>
2025-11-12 05:11:26 +0700

    Deploy Flutter Web

--- android/app/src/main/kotlin/com/example/api_anime/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.api_anime
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/constants/app_constants.dart
+++
@@ -1,6 +0,0 @@
-class AppConstants {
-  static const String appName = 'AnimInfo';
-  static const String splashDuration = '3';
-  static const String userKey = 'user_key';
-  static const String favoritesKey = 'favorites_key';
-}
